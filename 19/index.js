03f2e2d9b8f6456839cc28cfb3a086ce7068ef23
anothervictoria <victoria.panidi@gmail.com>
2024-05-04 16:10:20 +0400

    number guessing game

--- 19/index.js
+++ 19/index.js
@@ -40,15 +40,49 @@
 //_-----------------------------------------------------------
 //------------------------------------------------------------
 // FOR LOOP
-for (let i = 0; i <= 2; i++) {
-  console.log(`Hello!`);
-}
+// for (let i = 0; i <= 2; i++) {
+//   console.log(`Hello!`);
+// }
 
-for (let i = 0; i <= 20; i++) {
-  if (i === 13) {
-    continue;
-    // break;
+// for (let i = 0; i <= 20; i++) {
+//   if (i === 13) {
+//     continue;
+//     // break;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//_-----------------------------------------------------------
+//------------------------------------------------------------
+// NUMBER GUESSING GAME
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+  guess = Number(guess);
+  if (isNaN(guess)) {
+    window.alert("Please enter a valid number");
+  } else if (guess < minNum || guess > maxNum) {
+    window.alert("Please enter a valid number");
   } else {
-    console.log(i);
+    attempts++;
+    if (guess < answer) {
+      window.alert("Too low! Try again!");
+    } else if (guess > answer) {
+      window.alert("Too high! Try again!");
+    } else {
+      window.alert(
+        `Correct! The answer was ${answer}. It took you ${attempts} attempts`
+      );
+      running = false;
+    }
   }
 }
