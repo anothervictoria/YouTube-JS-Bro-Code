2c158d1058ddb1be8aeb03efee13f188d44a60e9
anothervictoria <victoria.panidi@gmail.com>
2024-05-05 17:58:57 +0400

    rest parameters

--- 24/index.js
+++ 24/index.js
@@ -53,28 +53,84 @@
 // SPREAD OPERATOR
 // ... - spreads an array intp differrent elements
 
-let numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
 
-// let maximum = Math.max(numbers); //Numeric expressions to be evaluated.
-// console.log(maximum);
+// // let maximum = Math.max(numbers); //Numeric expressions to be evaluated.
+// // console.log(maximum);
 
-let maximum = Math.max(...numbers); //imagine like you are opening the box and taking out smth
-let minimum = Math.min(...numbers);
-console.log(maximum, minimum);
+// let maximum = Math.max(...numbers); //imagine like you are opening the box and taking out smth
+// let minimum = Math.min(...numbers);
+// console.log(maximum, minimum);
 
-let username = "another";
-let letters = [...username].join("-");
-console.log(letters);
+// let username = "another";
+// let letters = [...username].join("-");
+// console.log(letters);
 
-let fruits = ["apple", "orange", "banana", "peach", "avocado", "strawberry"];
-let newFruits = [...fruits]; //copies the elements of the array in the new array
-console.log(fruits);
-console.log(newFruits);
+// let fruits = ["apple", "orange", "banana", "peach", "avocado", "strawberry"];
+// let newFruits = [...fruits]; //copies the elements of the array in the new array
+// console.log(fruits);
+// console.log(newFruits);
 
-let vegetables = ["carrots", "celery", "potatoes"];
-let foods = [...fruits, ...vegetables, "eggs", "milk"]; //combine 2 or more arrays
-console.log(foods);
+// let vegetables = ["carrots", "celery", "potatoes"];
+// let foods = [...fruits, ...vegetables, "eggs", "milk"]; //combine 2 or more arrays
+// console.log(foods);
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // REST PARAMETERS
+
+const food1 = "pizza";
+const food2 = "humburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+const food5 = "ramen";
+
+function openFridge(...foods) {
+  //combines separate elements into an array
+  console.log(...foods); //same 3 dots divide an existing array into separate elements
+}
+
+function getFood(...foods) {
+  return foods;
+}
+
+openFridge(food1, food2, food3, food4, food5);
+
+const foods = getFood(food1, food2, food3, food4, food5);
+console.log(foods);
+
+//---------------------------------------------------------------------------------------
+
+function sum(...numbers) {
+  let result = 0;
+  for (let number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+// const total = sum(1, 2, 3, 4);
+// console.log(`Your total is $${total}`);
+
+function getAverage(...numbers) {
+  let result = 0;
+  for (let number of numbers) {
+    result += number;
+  }
+  console.log(result);
+  console.log(numbers);
+  return result / numbers.length;
+}
+
+const total = getAverage(75, 100, 85, 90, 50);
+console.log(`The average is ${total}`);
+
+//---------------------------------------------------------------------------------------
+
+const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
+
+function combineStrings(...strings) {
+  return strings.join(" ");
+}
+
+console.log(fullName);
