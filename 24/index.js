18ecbbf91c09fbeb141266a8ebed940ed9a835a4
anothervictoria <victoria.panidi@gmail.com>
2024-05-05 17:21:06 +0400

    arrays

--- 24/index.js
+++ 24/index.js
@@ -17,3 +17,37 @@ function convert() {
     result.textContent = "Select a unit";
   }
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//ARRAYS
+let fruits = ["apple", "orange", "banana", "peach", "avocado", "strawberry"];
+console.log(fruits);
+fruits[2] = "coconut";
+console.log(fruits);
+
+fruits.push("cherry"); //adds an elements at the end of the array
+fruits.pop(); //removes the last element of the array
+fruits.unshift("mango"); //adds an element to the beginning of the array
+fruits.shift(); //removes the first element of the array
+
+let numOfFruits = fruits.length;
+console.log(numOfFruits);
+
+let index = fruits.indexOf("apple");
+console.log(index);
+index = fruits.indexOf("cucumber"); //if elements doesn't exist in the array it returns -1
+console.log(index);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+fruits.sort(); //sort elements in an alphabetical order
+fruits.sort().reverse();
+
+for (let fruit of fruits) {
+  console.log(fruit);
+} //like a shortcut to display elements of an array
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// SPREAD OPERATOR
