08e01455ad7860c10cae148158066122278e06c5
anothervictoria <victoria.panidi@gmail.com>
2024-05-07 14:34:43 +0400

    forEach()

--- 30/index.js
+++ 30/index.js
@@ -44,3 +44,61 @@ function displayPage(result) {
 }
 
 sum(displayPage, 3, 4);
+
+//--------------------------------------------------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// // numbers.forEach(display);
+
+// function display(element) {
+//   console.log(element);
+// }
+
+// function double(element, index, array) {
+//   array[index] = element * 2;
+// }
+
+// function triple(element, index, array) {
+//   array[index] = element * 3;
+// }
+
+// numbers.forEach(double);
+// numbers.forEach(display);
+
+// numbers.forEach(triple);
+// numbers.forEach(display);
+
+//--------------------------------------------------------
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+function display(element) {
+  console.log(element);
+}
+
+fruits.forEach(display);
+
+function upperCase(element, index, array) {
+  array[index] = element.toUpperCase();
+}
+
+function lowerCase(element, index, array) {
+  array[index] = element.toLowerCase();
+}
+
+fruits.forEach(upperCase);
+fruits.forEach(display);
+
+fruits.forEach(lowerCase);
+fruits.forEach(display);
+
+function capitalize(element, index, array) {
+  array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+fruits.forEach(capitalize);
+fruits.forEach(display);
